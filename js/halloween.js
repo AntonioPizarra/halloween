a68803f7ca74138464c8f48168aca7a6ab6d4fb6
Antonio González <agmpizarra25@gmail.com>
2023-11-25 13:51:11 +0100

    .

---
+++ js/halloween.js
@@ -0,0 +1,22 @@
+/**************************************************
+ * js functions for halloween exercise
+ * author: Antonio González Mayorga
+ * date: november 2023
+ * comment: my first exercise with js functions
+**************************************************/
+
+function show(element){
+    document.getElementById(element).style.visibility="visible"
+}
+
+function hide(element){
+    document.getElementById(element).style.visibility="hidden"
+}
+
+function sound(element){
+    document.getElementById(element).play()
+}
+
+function silence(element){
+    document.getElementById(element).pause();
+}
